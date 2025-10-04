c810294f50692f9b855001a4636f192c2e37857b
Manuel Suarez <82424347+ManuelSuarez93@users.noreply.github.com>
2025-10-04 11:29:39 -0300

    Added player character prefab, movement and input system

---
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerCharacter : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.AI.NavMeshAgent agent;
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private float speed = 1f;
+
+    public void FixedUpdate()
+    {
+        if (playerInput.actions["Move"].IsPressed())
+        {
+            Vector3 inputVector = playerInput.actions["Move"].ReadValue<Vector2>() * (Time.fixedDeltaTime * speed);
+            agent.Move(new Vector3(inputVector.x,0,inputVector.y));
+        } 
+        
+    }
+
+}
